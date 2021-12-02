81fabd2aa88c63c273c3ca82bbf5807872b8fe87
Alex Potter <CopperPenquin96@gmail.com>
2021-12-01 21:13:35 -0500

    SpawnPainting and SpawnPlayer

--- Redstone/Network/Packets/Play/Clientbound/SpawnPainting.cs
+++ Redstone/Network/Packets/Play/Clientbound/SpawnPainting.cs
@@ -13,23 +13,29 @@ namespace Redstone.Network.Packets.Play.Clientbound
         public Packet Packet => Packet.SpawnPainting;
         public string Name => "Spawn Painting";
 
-        private Position _pos;
-        private int _max;
+        private Painting _painting;
+        private Direction _dir;
 
         /// <summary>
         /// Initializes the packet.
         /// </summary>
         /// <param name="pos">The top left corner where the painting will be.</param>
         /// <param name="maxAmount">The max amount of squares available</param>
-        public SpawnPainting(Position pos, int maxAmount)
+        public SpawnPainting(Painting pos, Direction direction)
         {
-            _pos = pos;
-            _max = maxAmount;
+            _painting = pos;
+            _dir = direction;
         }
 
         public void Send(ref MinecraftClient client, GameStream stream)
         {
-            
+            int x = Math.Max(0, _painting.Width / 2 - 1);
+            int y = _painting.Height / 2;
+
+            Protocol.Send(ref client, stream, Packet,
+                _painting.Id, _painting.UniqueId.ToString(),
+                (VarInt) _painting.PaintingType,
+                new Position(x, y, 0), _dir);
         }
     }
 }
---
+++ Redstone/Network/Packets/Play/Clientbound/SpawnPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Redstone.Players;
+using Redstone.Types.Exceptions;
+
+namespace Redstone.Network.Packets.Play.Clientbound
+{
+    /// <summary>
+    /// To be used when a player comes into visible range,
+    /// not when a player joins.
+    /// </summary>
+    internal class SpawnPlayer : ISendingPacket
+    {
+        public Packet Packet => Packet.SpawnPlayer;
+        public string Name => "Spawn Player";
+
+        private Player _player;
+
+        public SpawnPlayer(Player player)
+        {
+            _player = player ?? throw new ArgumentNullException(nameof(player));
+        }
+
+        public void Send(ref MinecraftClient client, GameStream stream)
+        {
+            Protocol.Send(ref client, stream, Packet,
+                _player.Id, _player.UniqueId,
+                _player.Position.X,
+                _player.Position.Y,
+                _player.Position.Z,
+                _player.Position.Yaw, _player.Position.Pitch);
+        }
+    }
+}
